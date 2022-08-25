fc3b392701f33555c59b7f48c71012038086a687
Roman <ze6est1@yandex.ru>
2022-08-25 17:03:13 +0300

    fixed errors
    fixed Player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,65 +1,30 @@
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _maxHealth = 100f;
-    [SerializeField] private float _maxDelta = 10f;
-    [SerializeField] private float _currentHealth;
-    [SerializeField] private HealthBar _healthBar;
-    [SerializeField] private Button _buttonHealing;
-    [SerializeField] private Button _buttonDamage;
+    [SerializeField] private float _maxHealth = 100f;    
+    [SerializeField] private float _currentHealth;    
 
     private float _minHealth = 0;
 
     public float MaxHealth => _maxHealth;
-    public UnityAction<float> OnHealthGhanged;
-
-    private event UnityAction OnClickHealing
-    {
-        add => _buttonHealing.onClick.AddListener(value);
-        remove => _buttonHealing.onClick.RemoveListener(value);
-    }
-
-    private event UnityAction OnClickDamage
-    {
-        add => _buttonDamage.onClick.AddListener(value);
-        remove => _buttonDamage.onClick.RemoveListener(value);
-    }
-
-    private void OnEnable()
-    {
-        OnClickHealing += IncreaseHealth;
-        OnClickDamage += DecreaseHealth;
-    }
-
-    private void OnDisable()
-    {
-        OnClickHealing -= IncreaseHealth;
-        OnClickDamage -= DecreaseHealth;
-    }
+    public event UnityAction<float> OnHealthGhanged;
 
     private void Start()
     {        
         _currentHealth = _maxHealth;
     }
 
-    private void IncreaseHealth()
+    public void Heal(float numberLives)
     {
-        if (_currentHealth < _maxHealth)
-        {
-            _currentHealth += _maxDelta;
-            OnHealthGhanged.Invoke(_currentHealth);
-        }
+        _currentHealth = Mathf.Clamp(_currentHealth + numberLives, _minHealth, _maxHealth);
+        OnHealthGhanged.Invoke(_currentHealth);        
     }
 
-    private void DecreaseHealth()
+    public void TakeDamage(float damage)
     {
-        if (_currentHealth > _minHealth)
-        {
-            _currentHealth -= _maxDelta;
-            OnHealthGhanged.Invoke(_currentHealth);
-        }            
+        _currentHealth = Mathf.Clamp(_currentHealth - damage, _minHealth, _maxHealth);
+        OnHealthGhanged.Invoke(_currentHealth);
     }
 }
